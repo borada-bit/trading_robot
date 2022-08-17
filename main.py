9423720f1541a21e5a0a753a4105242f34edb585
Matas Kaminskas <matas.kaminskas@protonmail.com>
2022-08-17 21:32:50 +0300

    [ADD] make order function

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
+from typing import Optional
+
 from binance import Client, exceptions
 from binance.enums import *
 import config
 
 
-def main():
-    client = Client(config.api_key, config.api_secret)
+def init_client() -> Optional[Client]:
+    client = Client(config.api_key, config.api_secret, testnet=True)
     try:
         client.ping()
+        return client
     except exceptions.BinanceAPIException as e:
-        print(f"Error connecting to client. Error code :{e.status_code}. Message: {e.message}")
+        print(f"Error connecting to client. Error code :{e.status_code}. Message: {e.message} Code: {e.code}")
+        return None
 
+
+def make_order_at_price(client: Client, symbol: str, qty: float, price: float) -> bool:
+    order_completed = False
     try:
-        # https://dev.binance.vision/t/what-does-the-percent-price-filter-mean/134
-        info = client.get_symbol_info('ADAEUR')
-        info_price = client.get_symbol_ticker(symbol='ADAEUR')
-        # print(info_price)
-        # print(info['PERCENT_PRICE'])
-        # print(info['filters'][1])
-        val1 = info_price['price']
-        val2 = info['filters'][1]['multiplierDown']
-        print(f"{val1=} and {val2=}")
-        # print(price)
-
-        buy_loweset_price = float(val1) * float(val2)
-        round(buy_loweset_price, 5)
-        print(buy_loweset_price)
-
-        order = client.create_test_order(
-            symbol='ADAEUR',
+        order = client.create_order(
+            symbol=symbol,
             side=SIDE_BUY,
             type=ORDER_TYPE_LIMIT,
             timeInForce=TIME_IN_FORCE_GTC,
-            quantity=100,
-            price="0.5084")
-            # price=str(buy_loweset_price))
+            quantity=qty,
+            price=price)
 
-        print(order)
+        order_completed = True
     except exceptions.BinanceAPIException as e:
-        print(f"Error placing test order. Message: {e.message}")
+        print(f"Error executing order {e.message}")
 
-    # print(client.get_account())
-    # print(client.get_asset_balance("ada"))
-    # print(client.get_exchange_info())
-    # cia yra visa info kaip vadinasi tradinami simboliai
-    # print(client.get_orderbook_tickers())
+    return order_completed
+
+def main():
+    client = init_client()
+
 
 
 # Press the green button in the gutter to run the script.
