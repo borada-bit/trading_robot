25eb36cbb81def5d317b65816b533d295b711949
Matas Kaminskas <matas.kaminskas@protonmail.com>
2022-08-21 22:33:50 +0300

    [ADD] tradeable pairs to config

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ api_key = "key"
 api_secret = "secret"
 # specifies how much time to spend idling/waiting for command in main bot loop
 TIMEOUT = 60
+
+
+def get_pairs() -> list:
+    return ['BTCBUSD', 'LTCBUSD', 'XRPBUSD', 'BNBBTC', 'ETHBTC']
